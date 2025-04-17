8a4f36eebff75e1efeb77aa850cfd3c6abcf0289
Roberto Rojas <roberto.rojasp06@gmail.com>
2025-04-17 10:28:17 -0400

    Finish rendering of volumes.
    - TODO: add images with contours of GT and predicted
    masks for an specific slice.

--- get_images_nnunet_results.py
+++ get_images_nnunet_results.py
@@ -2,195 +2,176 @@ import numpy as np
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 from pathlib import Path
-from vedo import Volume, Plotter, show
-from PIL import Image
-
-from utils.plot import overlay_segmentation
-from utils.windowing import WINDOWS
+from vedo import Volume, Plotter
+from tqdm import tqdm
 
 
-def visualize_all_normalized(path_to_ct, path_to_mask):
-    # CT Volume
-    vol = Volume(path_to_ct)
-    vol_mesh = vol.isosurface()
-    vol_mesh.c("ivory").alpha(0.25).lighting("glossy")  # appearance settings
-    # Shrink the bounding box a bit
-    xmin, xmax, ymin, ymax, zmin, zmax = vol_mesh.bounds()
-    vol_mesh.cut_with_box([
-        xmin + 20, xmax - 20,
-        ymin + 70, ymax - 70,
-        zmin + 20, zmax - 20
-    ])
-    vol_mesh = vol_mesh.extract_largest_region()
-    # Mask Volume
-    mask_mesh = Volume(path_to_mask).isosurface(0.5)
-    mask_mesh.c("red").alpha(0.95).lighting('plastic')
-    mask_mesh = mask_mesh.smooth(niter=15, boundary=True)
-
-    # Show the volume
-    plotter = Plotter(bg="black")
-    plotter.show(
-        vol_mesh,
-        mask_mesh,
-        elevation=90,
-        axes=0,
-        zoom=True,
-        size=[512, 512]
-    )
-    plotter.zoom(1.35)
-    plotter.screenshot('output_image.png', scale=2)
-
-
-class Projector:
+class Visualizer:
     def __init__(self, path_to_output):
         self.path_to_output = path_to_output
-        self.figsize = (8, 8)
-        self.dpi = 300
-        self.epsilon = 1e-6
+        self.vol_mesh_color = 'ivory'
+        self.vol_mesh_alpha = 0.25
+        self.vol_mesh_lighting = 'glossy'
+        self.vol_mesh_bbox_xmin_delta = 20
+        self.vol_mesh_bbox_xmax_delta = 20
+        self.vol_mesh_bbox_ymin_delta = 70
+        self.vol_mesh_bbox_ymax_delta = 70
+        self.vol_mesh_bbox_zmin_delta = 20
+        self.vol_mesh_bbox_zmax_delta = 20
+        self.mask_mesh_color_gt = 'red'
+        self.mask_mesh_color_prediction = 'yellow'
+        self.mask_mesh_alpha = 0.95
+        self.mask_mesh_lighting = 'plastic'
+        self.mask_mesh_smooth_niter = 15
+        self.mask_mesh_smooth_boundary = True
+        self.output_image_size =(512, 512)
+        self.zoom_factor = 1.35
+        self.screenshot_scale = 2
+        self.background_color = 'black'
+        self.interactive = False
 
     @property
-    def path_to_ct_mip(self):
-        return Path(self.path_to_output) / "mip_ct"
+    def path_to_output_vols(self):
+        return Path(self.path_to_output) / "ct"
 
     @property
-    def path_to_overlaid_mip(self):
-        return Path(self.path_to_output) / "mip_overlaid"
+    def path_to_output_gt_overlaid(self):
+        return Path(self.path_to_output) / "ct-gt-overlaid"
 
     @property
-    def path_to_overlaid_masked_mip(self):
-        return Path(self.path_to_output) / "mip_overlaid_masked"
+    def path_to_output_prediction_overlaid(self):
+        return Path(self.path_to_output) / "ct-prediction-overlaid"
 
-    def _get_masked_overlay(self, ct_array, gt_array, mask_alpha=0.8,
-                            gray_alpha=0.5, window=None):
-        if window:
-            lower_bound = window["L"] - window["W"] / 2
-            upper_bound = window["L"] + window["W"] / 2
-            clipped_ct_array = np.where(
-                (ct_array >= lower_bound) & (ct_array <= upper_bound),
-                ct_array,
-                0
-            )
-            ct_mip = np.max(
-                # np.clip(ct_array, lower_bound, upper_bound),
-                clipped_ct_array,
-                axis=1
-            )
-            plt.imshow(clipped_ct_array[100])
-            plt.show()
-        else:
-            ct_mip = np.max(ct_array, axis=1)
-        gt_mip = np.max(gt_array, axis=1)
-        masked_ct_mip = np.max(
-            np.where(gt_array, ct_array, 0),
-            axis=1
-        )
-        masked_ct_mip_3c = np.stack(3 * [np.zeros_like(ct_mip)], axis=-1)
-        masked_ct_mip_3c[..., 1] = (masked_ct_mip - np.min(masked_ct_mip)) / (np.max(masked_ct_mip) - np.min(masked_ct_mip) + self.epsilon) # green
-        ct_mip_3c = np.stack(
-            3 * [(ct_mip - np.min(ct_mip)) / (np.max(ct_mip) - np.min(ct_mip) + self.epsilon)],
-            axis=-1
-        )
-        overlaid = np.where(
-            gt_mip[..., None] != 0,
-            (1 - mask_alpha ) * ct_mip_3c + mask_alpha * masked_ct_mip_3c,
-            gray_alpha * ct_mip_3c
-        )
-        return overlaid
-
-    def _project_volumes(self, path_to_ct, path_to_gt,
-                         path_to_prediction):
+    def render(self, path_to_ct, path_to_gt_mask, path_to_prediction_mask):
         for path in (
-            self.path_to_ct_mip,
-            self.path_to_overlaid_mip,
-            self.path_to_overlaid_masked_mip
+            self.path_to_output_vols,
+            self.path_to_output_gt_overlaid,
+            self.path_to_output_prediction_overlaid
         ):
             Path(path).mkdir(exist_ok=True)
-        # Read images
-        ct_image = sitk.ReadImage(path_to_ct)
-        ct_array = sitk.GetArrayFromImage(ct_image)
-        gt_array = sitk.GetArrayFromImage(sitk.ReadImage(path_to_gt))
-        prediction_array = sitk.GetArrayFromImage(sitk.ReadImage(path_to_prediction))
-        # Get masked CT
-        masked_ct_array = np.where(
-            gt_array,
-            ct_array,
-            0
+        # CT Volume as a mesh
+        vol = Volume(path_to_ct)
+        vol_mesh = vol.isosurface()
+        vol_mesh.c(self.vol_mesh_color).alpha(self.vol_mesh_alpha).lighting(self.vol_mesh_lighting)
+        # Shrink the bounding box a bit
+        xmin, xmax, ymin, ymax, zmin, zmax = vol_mesh.bounds()
+        vol_mesh.cut_with_box([
+            xmin + self.vol_mesh_bbox_xmin_delta, xmax - self.vol_mesh_bbox_xmax_delta,
+            ymin + self.vol_mesh_bbox_ymin_delta, ymax - self.vol_mesh_bbox_ymax_delta,
+            zmin + self.vol_mesh_bbox_zmin_delta, zmax - self.vol_mesh_bbox_zmax_delta
+        ])
+        vol_mesh = vol_mesh.extract_largest_region()
+        # Mask Volume as a mesh
+        mask_meshes = {
+            "gt": {
+                "path": path_to_gt_mask,
+                "path_to_output_folder": self.path_to_output_gt_overlaid,
+                "color": self.mask_mesh_color_gt,
+                "mesh": None
+            },
+            "prediction": {
+                "path": path_to_prediction_mask,
+                "path_to_output_folder": self.path_to_output_prediction_overlaid,
+                "color": self.mask_mesh_color_prediction,
+                "mesh": None
+            }
+        }
+        for mask_key, mask_value in mask_meshes.items():
+            mask_meshes[mask_key]["mesh"] = Volume(mask_value["path"]).isosurface(0.5)
+            mask_meshes[mask_key]["mesh"].c(mask_value["color"])
+            mask_meshes[mask_key]["mesh"].alpha(self.mask_mesh_alpha)
+            mask_meshes[mask_key]["mesh"].lighting(self.mask_mesh_lighting)
+            mask_meshes[mask_key]["mesh"].smooth(
+                niter=self.mask_mesh_smooth_niter,
+                boundary=self.mask_mesh_smooth_boundary
+            )
+        # Define common rendering parameters
+        cam = {
+            "elevation": 90,
+            "axes": 0,
+            "zoom": True,
+            "size": self.output_image_size,
+            "interactive": self.interactive
+        }
+        # Show the CT mesh alone
+        plotter = Plotter(
+            bg=self.background_color,
+            offscreen=True if not self.interactive else False
         )
-        # Get MIP projections
-        ct_mip = np.max(ct_array, axis=1)
-        masked_ct_mip = np.max(masked_ct_array, axis=1)
-        gt_mip = np.max(gt_array, axis=1)
-        prediction_mip = np.max(prediction_array, axis=1)
-        # print(f"gt foreground label: {[item for item in np.unique(gt_mip) if item != 0]}")
-        # print(f"prediction foreground label: {[item for item in np.unique(prediction_mip) if item != 0]}")
-        # Save MIP: CT
-        plt.figure(figsize=(8, 8))
-        plt.imshow(ct_mip, cmap='gray')
-        plt.axis('off')
-        plt.savefig(
-            Path(self.path_to_ct_mip) / f"{Path(path_to_ct).name.split('.nii.gz')[0]}.png",
-            transparent=True,
-            bbox_inches='tight',
-            dpi=self.dpi
+        plotter.show(vol_mesh, **cam)
+        plotter.zoom(self.zoom_factor)
+        plotter.screenshot(
+            Path(self.path_to_output_vols) / f"{Path(path_to_ct).name.split('.nii.gz')[0]}.png",
+            scale=self.screenshot_scale
         )
-        plt.close()
-        # Save MIP: overlaid mask
-        overlaid_mip = overlay_segmentation(
-            ct_mip,
-            prediction_mip,
-            alpha=0.25
-        )
-        plt.figure(figsize=(8, 8))
-        plt.imshow(overlaid_mip)
-        plt.axis('off')
-        plt.savefig(
-            Path(self.path_to_overlaid_mip) / f"{Path(path_to_ct).name.split('.nii.gz')[0]}.png",
-            transparent=True,
-            bbox_inches='tight',
-            dpi=self.dpi
-        )
-        plt.close()
+        plotter.close()
+        # Show the CT mesh overlaid with masks
+        for mask_value in mask_meshes.values():
+            plotter = Plotter(
+                bg=self.background_color,
+                offscreen=True if not self.interactive else False
+            )
+            plotter.show(vol_mesh, mask_value["mesh"], **cam)
+            plotter.zoom(self.zoom_factor)
+            plotter.screenshot(
+                Path(mask_value["path_to_output_folder"]) / f"{Path(path_to_ct).name.split('.nii.gz')[0]}.png",
+                scale=self.screenshot_scale
+            )
+            plotter.close()
 
-        visualize_all(path_to_ct, path_to_gt)
+    def render_all(self, path_to_cts, path_to_gts, path_to_predictions):
+        paths_to_gts = sorted(list(Path(path_to_gts).glob('*.nii.gz')))
+        paths_to_predictions = sorted(list(Path(path_to_predictions).glob('*.nii.gz')))
+        paths_to_cts = sorted(
+            [
+                Path(path_to_cts) / f"{path.name.split('.nii.gz')[0]}_0000.nii.gz"
+                for path in paths_to_gts
+            ]
+        )
+        for path_to_ct, path_to_gt, path_to_prediction in tqdm(
+            zip(
+                paths_to_cts,
+                paths_to_gts,
+                paths_to_predictions
+            ),
+            total=len(paths_to_cts)
+        ):
+            tqdm.write(f"ct: {Path(path_to_ct).name}")
+            self.render(
+                str(path_to_ct),
+                str(path_to_gt),
+                str(path_to_prediction)
+            )
 
 
-def test_projection():
-    path_to_ct = "/media/robber/Nuevo vol/recist-dataset/data/images/final/test-updated-beta/images/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660.nii.gz"
-    # path_to_ct = "/media/robber/Nuevo vol/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/imagesTs/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660_0000.nii.gz"
-    path_to_gt = "/media/robber/Nuevo vol/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/labelsTs/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660.nii.gz"
-    path_to_prediction = "/media/robber/Nuevo vol/nnUNet/results/inference/Dataset536_HCUCH_FineTuningLung_draft/best_chk/3d_fullres_lr_0_0001/ensemble/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660.nii.gz"
-    path_to_output = "/home/robber/mip_projections"
-    # projector = Projector(path_to_output)
-    # projector._project_volumes(
-    #     path_to_ct,
-    #     path_to_gt,
-    #     path_to_prediction
-    # )
-    visualize_all(
+def test_sample():
+    path_to_ct = "/media/cosmo/Data/fondef_ID23I10337/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/imagesTs/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660_0000.nii.gz"
+    path_to_gt = "/media/cosmo/Data/fondef_ID23I10337/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/labelsTs/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660.nii.gz"
+    path_to_prediction = "/media/cosmo/Data/fondef_ID23I10337/nnUNet/results/inference/Dataset536_HCUCH_FineTuningLung_draft/best_chk/3d_fullres_lr_0_0001/ensemble/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660.nii.gz"
+    path_to_output = "/home/cosmo/nnunet-renders"
+    visualizer = Visualizer(path_to_output)
+    visualizer.render(
         path_to_ct,
-        path_to_gt
+        path_to_gt,
+        path_to_prediction
     )
 
 
-def test_projection_normalized():
-    path_to_ct = "/media/robber/Nuevo vol/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/imagesTs/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660_0000.nii.gz"
-    path_to_gt = "/media/robber/Nuevo vol/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/labelsTs/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660.nii.gz"
-    path_to_prediction = "/media/robber/Nuevo vol/nnUNet/results/inference/Dataset536_HCUCH_FineTuningLung_draft/best_chk/3d_fullres_lr_0_0001/ensemble/1.3.12.2.1107.5.1.4.83504.30000021071911510369800019660.nii.gz"
-    path_to_output = "/home/robber/mip_projections"
-    # projector = Projector(path_to_output)
-    # projector._project_volumes(
-    #     path_to_ct,
-    #     path_to_gt,
-    #     path_to_prediction
-    # )
-    visualize_all_normalized(
-        path_to_ct,
-        path_to_gt
+def test_all():
+    path_to_cts = "/media/cosmo/Data/fondef_ID23I10337/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/imagesTs"
+    path_to_gts = "/media/cosmo/Data/fondef_ID23I10337/nnUNet/data/raw/Dataset536_HCUCH_FineTuningLung_draft/labelsTs"
+    path_to_predictions = "/media/cosmo/Data/fondef_ID23I10337/nnUNet/results/inference/Dataset536_HCUCH_FineTuningLung_draft/best_chk/3d_fullres_lr_0_0001/ensemble"
+    path_to_output = "/home/cosmo/nnunet-renders"
+    visualizer = Visualizer(path_to_output)
+    visualizer.render_all(
+        path_to_cts,
+        path_to_gts,
+        path_to_predictions
     )
 
 
 def main():
-    test_projection_normalized()
+    test_all()
 
 
 if __name__ == "__main__":
